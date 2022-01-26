d4be72c0e2d5b50f025aa64ce014e1d0ef3f69a5
Janer <janer.torres@udea.edu.co>
2022-01-26 11:59:59 -0500

    Creacion AutomataController.java

---
+++ src/main/java/com/udea/co/practicaitdl/AutomataController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.udea.co.practicaitdl;
+
+/**
+ *
+ * @author naides
+ */
+public class AutomataController {
+    
+}
